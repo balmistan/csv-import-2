f6cecc1496a8e22acfd73ac31c5c89a92d2e5d2c
Daniele <sangiovanni.daniele@gmail.com>
2017-04-05 08:20:01 +0200

    added new files

---
+++ pages/language_read.php
@@ -0,0 +1,25 @@
+<?php
+
+if(isset($_GET["lang"])){
+    switch($_GET["lang"]){
+        case "it":
+            $language = "it";
+            break;
+        case "de":
+            $language = "de";
+            break;
+        default:
+            //it is in config.php defined
+            break;
+    }
+}
+
+$page = file_get_contents("../lang/lang_".$language.".json");
+$ltext = json_decode($page, true);
+$button = $ltext["button"];
+$label = $ltext["label"];
+$msg = $ltext["msg"];
+$error = $ltext["error"];
+$legend = $ltext["legend"];
+$option_select = $ltext["option_select"];
+$info = $ltext["info"];
