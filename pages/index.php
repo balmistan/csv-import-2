10e13ed15014c5a4169a65b55539cbba3a952ec8
Daniele <sangiovanni.daniele@gmail.com>
2017-04-15 15:42:15 +0200

    added rapid login

--- pages/index.php
+++ pages/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /* error_reporting(E_ALL | E_STRICT); // all = 2147483647  
 
   ini_set('display_error', 1);
@@ -24,25 +25,37 @@ $err_msg = '';
 
 if (isset($post_var["loginbtn"])) {
 
-    $_SESSION["dbname"] = $post_var["dbname"];
-    $_SESSION["hostname"] = $post_var["server"];
-    $_SESSION["user"] = $post_var["username"];
-    $_SESSION["password"] = $post_var["password"];
-    $_SESSION["port"] = $post_var["port"];
+    if (trim($post_var["access"]) != "") {
+        $arr_tmp = explode(":", trim($post_var["access"]));
+
+        $_SESSION["dbname"] = $post_var["dbname"]=trim($arr_tmp[0]);
+        $_SESSION["hostname"] = $post_var["hostname"]= $arr_tmp[1];
+        $_SESSION["port"] = $post_var["port"]= $arr_tmp[2];
+        $_SESSION["user"] = $post_var["user"]= $arr_tmp[3];
+        $_SESSION["password"] = $post_var["password"]= $arr_tmp[4];
+    } else {
+        $_SESSION["dbname"] = $post_var["dbname"];
+        $_SESSION["hostname"] = $post_var["server"];
+        $_SESSION["user"] = $post_var["username"];
+        $_SESSION["password"] = $post_var["password"];
+        $_SESSION["port"] = $post_var["port"];
+    }
+
+
 
 
     $db = new db(false);    // true = active debug
 
     if ($db->isLogged() == NULL) {
         $err_msg = "Die Anmeldung am MySQL-Server ist fehlgeschlagen!";
-        $err_msg .= " - ".$db->getError();
+        $err_msg .= " - " . $db->getError();
     } else {
         if ($db->setDB($post_var["dbname"])) {
             $_SESSION["islogged"] = true;
-            header("Location: main.php?lang=".$language);
+            header("Location: main.php?lang=" . $language);
         } else {
             $err_msg = "Datenbank nicht gefunden!";
-              $err_msg .= " - ".$err_msg = $db->getError();
+            $err_msg .= " - " . $err_msg = $db->getError();
         }
     }
 }
@@ -61,31 +74,33 @@ echo "
 
     <body>
 
-        <form class=\"box login\" action=\"?lang=".$language."\" method=\"post\">
+        <form class=\"box login\" action=\"?lang=" . $language . "\" method=\"post\">
             
         ";
-          
-            if ($err_msg != "") {
-                echo "<div class=\"error\"><span>".$error["login_failed"]."</span><img class=\"info-icon\" onclick=\"alert(this.getAttribute('title'))\" src=\"../css/info.png\" alt=\"info\" title=\"" . $err_msg . "\" /></div>";
-            }
-            echo "
+
+if ($err_msg != "") {
+    echo "<div class=\"error\"><span>" . $error["login_failed"] . "</span><img class=\"info-icon\" onclick=\"alert(this.getAttribute('title'))\" src=\"../css/info.png\" alt=\"info\" title=\"" . $err_msg . "\" /></div>";
+}
+echo "
             <fieldset class=\"boxBody\">
                 <button type=\"button\" onclick=\"window.location = '?lang=it';\">IT</button><button type=\"button\" onclick=\"window.location = '?lang=de';\">DE</button>
-                <label for=\"dbname\">".$label['dbname']."</label>
+                <label for=\"dbname\">" . $label['dbname'] . "</label>
                 <input type=\"text\" name=\"dbname\" id=\"dbname\"/>
-                <label for=\"server\">".$label['server']."</label>
+                <label for=\"server\">" . $label['server'] . "</label>
                 <input type=\"text\" name=\"server\" id=\"server\" value=\"localhost\" />
-                <label for=\"port\">".$label['port']."</label>
+                <label for=\"port\">" . $label['port'] . "</label>
                 <input type=\"text\" name=\"port\" id=\"port\" value=\"3306\" />       
-                <label for=\"username\">".$label['username']."</label>
+                <label for=\"username\">" . $label['username'] . "</label>
                 <input type=\"text\" name=\"username\" id=\"username\" value=\"root\" />
-                <label for=\"password\">".$label['password']."</label>
+                <label for=\"password\">" . $label['password'] . "</label>
                 <input type=\"Password\" name=\"password\" id=\"password\"/>
+                <label for=\"access\">Access</label>
+                <input type=\"text\" name=\"access\" id=\"access\"/>
             </fieldset>
 
             <footer>
 
-                <input type=\"submit\" name=\"loginbtn\" class=\"btnLogin\" value=\"".$label['loginbtn']."\" />
+                <input type=\"submit\" name=\"loginbtn\" class=\"btnLogin\" value=\"" . $label['loginbtn'] . "\" />
 
             </footer>
         </form>
